e901926bba2340e79043336da69f114d22bbf3ce
Alexey Korablin <Alexey_Korablin@epam.com>
2019-04-11 14:59:56 +0300

    12_bop3 Task solving. Improved with zipfile

--- byte_of_python_ref/task_solving.py
+++ byte_of_python_ref/task_solving.py
@@ -7,7 +7,7 @@ import zipfile
 # Для выполненеия этой задачи в OS Windows нужно установить программу GnuWin32
 
 source = ['D:\\loki\\legacy\\python3\\learnPython\\byte_of_python_ref\\data',
-          'D:\\loki\\legacy\\python3\\py3-simple_2d_game']
+          'D:\\loki\\legacy\\python3\\learnPython\\byte_of_python_ref\\logs']
 target_dir = 'D:\\loki\\legacy\\python3\\learnPython\\byte_of_python_ref\\backup'
 
 today = target_dir + os.sep + time.strftime('%Y%m%d')
@@ -26,8 +26,8 @@ if not os.path.exists(today):
 
 print('Folder has been created successfully', today)
 
-zip_command = 'zip -qr {0} {1}'.format(target, ' '.join(source))
-print(zip_command)
+# zip_command = 'zip -qr {0} {1}'.format(target, ' '.join(source))
+# print(zip_command)
 
 # os.system(<command>) -> исполняет системную команду и возвращает код ее завершения. 0 - успех
 # if os.system(zip_command) == 0:
@@ -35,8 +35,20 @@ print(zip_command)
 # else:
 #     print('Backup creating was failed')
 
-for root, dirs, files in os.walk(source[1]):
-    print(root)
-    print(dirs)
-    print(files)
+z = zipfile.ZipFile(target, 'w')
+
+for folder in source:
+    for root, dirs, files in os.walk(folder):
+        print(root)
+        print(dirs)
+        print('files =>', files)
+        for file in files:
+            z.write(os.path.join(root, file))
+
+z.close()
+
+
+
+
+
 
